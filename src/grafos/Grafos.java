eed299a788e1003e93de0ae57aa6832c530adbda
Carlos Antillón <Carlos Antilln>
2016-05-20 19:58:52 -0500

    Menu, grafos

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -1,6 +1,4 @@
 package grafos;
-
-
 /**
  *
  * @author J. P. Toledo
@@ -14,24 +12,38 @@ public class Grafos {
      * Metodo Main de la clase que contiene la ventana principal
      */
     public static void main(String[] args) {
-        g.agregarEstado(new Nodo("Nodo1",1,1,"MiNodo"));
-        g.agregarEstado(new Nodo("Nodo2",1,1,"MiNodo2"));
-        g.agregarEstado(new Nodo("Nodo3",1,1,"MiNodo3"));
-        g.agregarEstado(new Nodo("Nodo4",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("A",1,1,"MiNodo"));
+        g.agregarEstado(new Nodo("B",1,1,"MiNodo2"));
+        g.agregarEstado(new Nodo("C",1,1,"MiNodo3"));
+        g.agregarEstado(new Nodo("D",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("E",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("F",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("G",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("H",1,1,"MiNodo4"));
         
-        g.enlace(g.estado("Nodo1"), g.estado("Nodo2"));
-        g.enlace(g.estado("Nodo1"), g.estado("Nodo3"));
-        g.enlace(g.estado("Nodo1"), g.estado("Nodo4"));
-        g.enlace(g.estado("Nodo2"), g.estado("Nodo3"));
+        g.enlace(g.estado("A"), g.estado("B"));
+        g.enlace(g.estado("A"), g.estado("C"));
+        g.enlace(g.estado("A"), g.estado("D"));
+        g.enlace(g.estado("B"), g.estado("E"));
+        g.enlace(g.estado("B"), g.estado("F"));
+        g.enlace(g.estado("C"), g.estado("E"));
+        g.enlace(g.estado("D"), g.estado("E"));
+        g.enlace(g.estado("D"), g.estado("F"));
+        g.enlace(g.estado("E"), g.estado("H"));
+        g.enlace(g.estado("F"), g.estado("H"));
+        g.enlace(g.estado("F"), g.estado("G"));
         
-        System.out.println("Lista de adyacencia Nodo 1");
+        /*System.out.println("Lista de adyacencia Nodo 1");
         g.elementosLista("Nodo1");
         System.out.println("Lista de adyacencia Nodo 2");
         g.elementosLista("Nodo2");
         
-        /*g.inicio.ListaAdy.removerNodo("Nodo2","Nodo1");
+        g.inicio.ListaAdy.removerNodo("Nodo2","Nodo1");
         System.out.println("Lista de adyacencia Nodo 1");
         g.elementosLista("Nodo1");*/
+        AlgoritmosCarlos obj = new AlgoritmosCarlos();
+        obj.ProEnProfundidad(g,g.estado("A"), g.estado("H"));
+        
     }
     
 }
