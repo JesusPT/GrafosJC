7b5c372f911cfa07aafb0a6d14841f9d52f28fcd
Jesús Pérez Toledo <shini.p.t.sis@gmail.com>
2016-05-20 20:02:55 -0500

    primero el guapo

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -1,5 +1,8 @@
 package grafos;
 
+import java.awt.Color;
+import java.util.Random;
+
 
 /**
  *
@@ -7,27 +10,48 @@ package grafos;
  */
 public class Grafos {
     static grafo g = new grafo("Lol");    
-    
+    static Random rand = new Random();
+    static jesus j = new jesus();
     
     /**
      * @param args the command line arguments
      * Metodo Main de la clase que contiene la ventana principal
      */
     public static void main(String[] args) {
-        g.agregarEstado(new Nodo("Nodo1",1,1,"MiNodo"));
-        g.agregarEstado(new Nodo("Nodo2",1,1,"MiNodo2"));
-        g.agregarEstado(new Nodo("Nodo3",1,1,"MiNodo3"));
-        g.agregarEstado(new Nodo("Nodo4",1,1,"MiNodo4"));
+        int i = 1;
+        g.agregarEstado(new Nodo("A", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+        i++;
+        g.agregarEstado(new Nodo("B", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+        i++;
+        g.agregarEstado(new Nodo("C", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+        i++;
+        g.agregarEstado(new Nodo("D", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+        i++;
+        g.agregarEstado(new Nodo("E", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+        i++;
+        
         
         g.enlace(g.estado("Nodo1"), g.estado("Nodo2"));
         g.enlace(g.estado("Nodo1"), g.estado("Nodo3"));
         g.enlace(g.estado("Nodo1"), g.estado("Nodo4"));
-        g.enlace(g.estado("Nodo2"), g.estado("Nodo3"));
+        g.enlace(g.estado("Nodo2"), g.estado("Nodo5"));
+        g.enlace(g.estado("Nodo3"), g.estado("Nodo5"));
+        g.enlace(g.estado("Nodo4"), g.estado("Nodo5"));
+        
+        g.estado("Nodo1").ListaAdy.getNodo("Nodo2").setHeuristica(3);
+        g.estado("Nodo1").ListaAdy.getNodo("Nodo3").setHeuristica(8);
+        g.estado("Nodo1").ListaAdy.getNodo("Nodo4").setHeuristica(1);
+        
+        g.estado("Nodo2").ListaAdy.getNodo("Nodo5").setHeuristica(7);
+        g.estado("Nodo3").ListaAdy.getNodo("Nodo5").setHeuristica(8);
+        g.estado("Nodo4").ListaAdy.getNodo("Nodo5").setHeuristica(3);
         
         System.out.println("Lista de adyacencia Nodo 1");
         g.elementosLista("Nodo1");
         System.out.println("Lista de adyacencia Nodo 2");
         g.elementosLista("Nodo2");
+        
+        j.primeroElMejor(g, g.estado("Nodo5"));
         
         /*g.inicio.ListaAdy.removerNodo("Nodo2","Nodo1");
         System.out.println("Lista de adyacencia Nodo 1");
