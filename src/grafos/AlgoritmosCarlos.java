7d85115145df2534e14ab168afcd8400b6783533
Carlos Antillón <Carlos Antilln>
2016-06-01 09:39:33 -0500

    Algoritmos Carlos

--- src/grafos/AlgoritmosCarlos.java
+++ src/grafos/AlgoritmosCarlos.java
@@ -10,8 +10,8 @@ package grafos;
  */
 import java.util.Stack;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 public class AlgoritmosCarlos {
-    
     public void Dijkstra(grafo g,Nodo origen){
         /*creamos una cola de prioridad Q
 3      agregamos origen a la cola de prioridad Q
@@ -29,50 +29,192 @@ public class AlgoritmosCarlos {
 3         distancia[ adyacente ] = distancia[ actual ] + peso
 4         agregamos adyacente a la cola de prioridad Q*/
     }
+   
+    public void RecorridoGuiado(grafo g,Nodo inicio, Nodo fin){
+        ArrayList<Nodo> hijos;
+        g.GetFin();
+        Nodo actual;
+        actual=inicio;
+        boolean ban=false;
+        while(!fin.getNombre().equals(actual.getNombre())){
+            if(g.estado(actual.Nombre).ListaAdy!=null){
+            hijos=g.estado(actual.Nombre).ListaAdy.getElementos();
+                for (int i = 0; i < hijos.size(); i++) {
+                    System.out.println(""+g.estado(hijos.get(i).Nombre).getNombre());
+                    if(g.estado(hijos.get(i).Nombre).getNombre().equals(fin.getNombre())){
+                        //System.out.println(""+g.estado(hijos.get(i).Nombre).getNombre());
+                        ban=true;
+                        break;
+                        
+                    }
+                }
+                if(!ban){
+                actual=g.estado(JOptionPane.showInputDialog("Ingrese el nodo por el que quiere irse"));
+                }
+                else{
+                    actual=fin;
+                }
+            }
+            else{
+                System.out.println("Fin del recorrido");
+                fin=actual;
+            }    
+        }
+    }
    public void ProEnProfundidad(grafo grafo, Nodo inicio, Nodo fin){
-       Stack<Nodo> EstadoAbierto = new Stack<Nodo>();
+          Stack<Nodo> EstadoAbierto = new Stack<Nodo>();
        Stack<Nodo> EstadoCerrado = new Stack<Nodo>();
-       Nodo actual;
+       Stack<Nodo> padres = new Stack<Nodo>();
+       String[][] resultado;
+       Nodo actual,papa;
        ArrayList<Nodo> hijos;
-       //try{
+       try{
         EstadoAbierto.push(inicio);
-        actual=EstadoAbierto.firstElement();
-        while(fin!=actual&&!EstadoAbierto.empty()){
+        papa=actual=EstadoAbierto.firstElement();
+        padres.push(actual);
+        while(!fin.getNombre().equals(actual.getNombre())&&!EstadoAbierto.empty()){
         EstadoAbierto.remove(EstadoAbierto.firstElement());
         EstadoCerrado.push(actual);
+        padres.push(papa);
+        if(grafo.estado(actual.Nombre).ListaAdy!=null){
         hijos=grafo.estado(actual.Nombre).ListaAdy.getElementos();
         hijos=TratarRepetidos(hijos,EstadoAbierto,EstadoCerrado);
-        //Insertar los hijos en el estado abierto
-            for (int i = 0; i < hijos.size(); i++) {
-                EstadoAbierto.push(hijos.get(i));
-            }
-        actual=EstadoAbierto.firstElement();
+        if(hijos.size()>0){
+            papa=actual;
         }
-        System.out.println("Estado cerrado:");
+        //Insertar los hijos en el estado abierto
+        for (int i = hijos.size()-1; i >=0 ; i--) {
+              EstadoAbierto.push(hijos.get(i));
+           }
+        }
+        actual=EstadoAbierto.peek();
+        }
+        EstadoCerrado.push(actual);
+        padres.push(papa);
+        resultado= new String[EstadoCerrado.size()][2];
+        int x=0;
         while(!EstadoCerrado.empty()){
-         System.out.println(""+(EstadoCerrado.pop()).Nombre);
-        }    
-       /*}
+         resultado[x][0]=(EstadoCerrado.pop()).Nombre;
+         resultado[x][1]=(padres.pop()).Nombre;
+         System.out.println(""+resultado[x][0]+" Padre: "+resultado[x][1]);
+         x++;
+        }
+        System.out.println("Resultado: "+GetResultado(resultado));
+        
+       }
        catch(Exception e){
            System.out.println("Error: "+e.toString());
-       }*/
+       }
        
    }
    
-   public void ProEnAnchura(){
-       
+   public void ProEnAnchura(grafo grafo, Nodo inicio, Nodo fin){
+       Stack<Nodo> EstadoAbierto = new Stack<Nodo>();
+       Stack<Nodo> EstadoCerrado = new Stack<Nodo>();
+       Stack<Nodo> padres = new Stack<Nodo>();
+       String[][] resultado;
+       Nodo actual,papa;
+       ArrayList<Nodo> hijos;
+       try{
+        EstadoAbierto.push(inicio);
+        papa=actual=EstadoAbierto.firstElement();
+        padres.push(actual);
+        while(!fin.getNombre().equals(actual.getNombre())&&!EstadoAbierto.empty()){
+        EstadoAbierto.remove(EstadoAbierto.firstElement());
+        EstadoCerrado.push(actual);
+        padres.push(papa);
+        if(grafo.estado(actual.Nombre).ListaAdy!=null){
+        hijos=grafo.estado(actual.Nombre).ListaAdy.getElementos();
+        hijos=TratarRepetidos(hijos,EstadoAbierto,EstadoCerrado);
+        if(hijos.size()>0){
+            papa=actual;
+        }
+        //Insertar los hijos en el estado abierto
+        for (int i = 0; i < hijos.size(); i++) {
+              EstadoAbierto.push(hijos.get(i));
+              
+           }
+        }
+        actual=EstadoAbierto.firstElement();
+        }
+        EstadoCerrado.push(actual);
+        resultado= new String[EstadoCerrado.size()][2];
+        int x=0;
+        while(!EstadoCerrado.empty()){
+         resultado[x][0]=(EstadoCerrado.pop()).Nombre;
+         resultado[x][1]=(padres.pop()).Nombre;
+         System.out.println(""+resultado[x][0]+" Padre: "+resultado[x][1]);
+         x++;
+        }
+        System.out.println("Resultado: "+GetResultado(resultado));
+        
+       }
+       catch(Exception e){
+           System.out.println("Error: "+e.toString());
+       }
+   }
+   public String GetResultado(String[][] valores){
+       Stack<String> res = new Stack();
+       String resultado="";
+       String fin=valores[valores.length-1][1];
+       String inicio=valores[0][1];
+       res.push(valores[0][0]);
+       while(!fin.equals(inicio)){
+           for(int i=0;i<valores.length;i++){
+               if(inicio.equals(valores[i][0])){
+                   res.push(inicio);
+                   inicio = valores[i][1];
+               }
+           }
+       }
+       while(!res.empty()){
+           resultado=resultado+res.pop();
+       }
+       return resultado;
    }
    public ArrayList TratarRepetidos(ArrayList hijos, Stack EstadoAbierto, Stack EstadoCerrado){
        Nodo hijo;
-       for (int i = 0; i < hijos.size(); i++) {
-         hijo=(Nodo)hijos.get(i);
-         if(EstadoAbierto.contains(hijo)){
-             hijos.remove(hijo);
-         }
-         if(EstadoCerrado.contains(hijo)){
-             hijos.remove(hijo);
-         }
+       Nodo fueraEA[]= new Nodo[EstadoAbierto.size()];
+       Nodo fueraEC[]= new Nodo[EstadoCerrado.size()];
+       int a=0,c;
+       c=hijos.size();
+       //Vaciar elementos de estados a arreglos para poderlos comparar:
+       while(!EstadoAbierto.empty()){
+            fueraEA[a]=(Nodo)EstadoAbierto.pop();
+            a++;
        }
+       a=0;
+       while(!EstadoCerrado.empty()){
+            fueraEC[a]=(Nodo)EstadoCerrado.pop();
+            a++;
+       }
+       //Comparar cada hijo con los elementos de los estados
+       for (int i = 0; i < c; i++) {
+         if(c!=hijos.size()){
+             c=c-1;
+             i=i-1;
+         }
+         hijo=(Nodo)hijos.get(i);
+         for(int j=0;j<fueraEA.length;j++){
+         if(fueraEA[j].getNombre().equals(hijo.getNombre())){
+                hijos.remove(hijo);
+            }    
+         }
+         for(int j=0;j<fueraEC.length;j++){
+         if(fueraEC[j].getNombre().equals(hijo.getNombre())){
+                hijos.remove(hijo);
+            }    
+         }
+        }
+       //Volver a meter los elementos  a EstadoAbierto:
+       int i;
+         for(i=fueraEA.length-1;i>=0;i--){ 
+             EstadoAbierto.push(fueraEA[i]);
+         }
+       //Volver a meter los elementos  a EstadoCerrado:
+         for(i=fueraEC.length-1;i>=0;i--){ 
+             EstadoCerrado.push(fueraEC[i]);
+         }
     return hijos;   
    }
 }
--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -17,54 +17,60 @@ public class Grafos {
      */
     public static void main(String[] args) {
 
-        int i = 1;
-        g.agregarEstado(new Nodo("A", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
-        i++;
-        g.agregarEstado(new Nodo("B", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
-        i++;
-        g.agregarEstado(new Nodo("C", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
-        i++;
-        g.agregarEstado(new Nodo("D", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
-        i++;
-        g.agregarEstado(new Nodo("E", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
-        i++;
-        
-        
-        g.enlace(g.estado("Nodo1"), g.estado("Nodo2"));
-        g.enlace(g.estado("Nodo1"), g.estado("Nodo3"));
-        g.enlace(g.estado("Nodo1"), g.estado("Nodo4"));
-        g.enlace(g.estado("Nodo2"), g.estado("Nodo5"));
-        g.enlace(g.estado("Nodo3"), g.estado("Nodo5"));
-        g.enlace(g.estado("Nodo4"), g.estado("Nodo5"));
-        
-        g.estado("Nodo1").ListaAdy.getNodo("Nodo2").setHeuristica(3);
-        g.estado("Nodo1").ListaAdy.getNodo("Nodo3").setHeuristica(8);
-        g.estado("Nodo1").ListaAdy.getNodo("Nodo4").setHeuristica(1);
-        
-        g.estado("Nodo2").ListaAdy.getNodo("Nodo5").setHeuristica(7);
-        g.estado("Nodo3").ListaAdy.getNodo("Nodo5").setHeuristica(8);
-        g.estado("Nodo4").ListaAdy.getNodo("Nodo5").setHeuristica(3);
-//
-//        g.agregarEstado(new Nodo("A",1,1,"MiNodo"));
-//        g.agregarEstado(new Nodo("B",1,1,"MiNodo2"));
-//        g.agregarEstado(new Nodo("C",1,1,"MiNodo3"));
-//        g.agregarEstado(new Nodo("D",1,1,"MiNodo4"));
-//        g.agregarEstado(new Nodo("E",1,1,"MiNodo4"));
-//        g.agregarEstado(new Nodo("F",1,1,"MiNodo4"));
-//        g.agregarEstado(new Nodo("G",1,1,"MiNodo4"));
-//        g.agregarEstado(new Nodo("H",1,1,"MiNodo4"));
+//        int i = 1;
+//        g.agregarEstado(new Nodo("A", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+//        i++;
+//        g.agregarEstado(new Nodo("B", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+//        i++;
+//        g.agregarEstado(new Nodo("C", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+//        i++;
+//        g.agregarEstado(new Nodo("D", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+//        i++;
+//        g.agregarEstado(new Nodo("E", 0, 0, "Nodo"+i,new Color(rand.nextInt(255)+1, rand.nextInt(255)+1, rand.nextInt(255)+1)));
+//        i++;
 //        
-//        g.enlace(g.estado("A"), g.estado("B"));
-//        g.enlace(g.estado("A"), g.estado("C"));
-//        g.enlace(g.estado("A"), g.estado("D"));
-//        g.enlace(g.estado("B"), g.estado("E"));
-//        g.enlace(g.estado("B"), g.estado("F"));
-//        g.enlace(g.estado("C"), g.estado("E"));
-//        g.enlace(g.estado("D"), g.estado("E"));
-//        g.enlace(g.estado("D"), g.estado("F"));
-//        g.enlace(g.estado("E"), g.estado("H"));
-//        g.enlace(g.estado("F"), g.estado("H"));
-//        g.enlace(g.estado("F"), g.estado("G"));
+//        
+//        g.enlace(g.estado("Nodo1"), g.estado("Nodo2"));
+//        g.enlace(g.estado("Nodo1"), g.estado("Nodo3"));
+//        g.enlace(g.estado("Nodo1"), g.estado("Nodo4"));
+//        g.enlace(g.estado("Nodo2"), g.estado("Nodo5"));
+//        g.enlace(g.estado("Nodo3"), g.estado("Nodo5"));
+//        g.enlace(g.estado("Nodo4"), g.estado("Nodo5"));
+//        
+//        g.estado("Nodo1").ListaAdy.getNodo("Nodo2").setHeuristica(3);
+//        g.estado("Nodo1").ListaAdy.getNodo("Nodo3").setHeuristica(8);
+//        g.estado("Nodo1").ListaAdy.getNodo("Nodo4").setHeuristica(1);
+//        
+//        g.estado("Nodo2").ListaAdy.getNodo("Nodo5").setHeuristica(7);
+//        g.estado("Nodo3").ListaAdy.getNodo("Nodo5").setHeuristica(8);
+//        g.estado("Nodo4").ListaAdy.getNodo("Nodo5").setHeuristica(3);
+//
+        /*g.agregarEstado(new Nodo("A",1,1,"MiNodo"));
+        g.agregarEstado(new Nodo("B",1,1,"MiNodo2"));
+        g.agregarEstado(new Nodo("C",1,1,"MiNodo3"));
+        g.agregarEstado(new Nodo("D",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("E",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("F",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("G",1,1,"MiNodo4"));
+        g.agregarEstado(new Nodo("H",1,1,"MiNodo4"));
+        
+        g.enlace(g.estado("A"), g.estado("B"));
+        g.enlace(g.estado("A"), g.estado("C"));
+        g.enlace(g.estado("A"), g.estado("D"));
+        g.enlace(g.estado("B"), g.estado("E"));
+        g.enlace(g.estado("B"), g.estado("F"));
+        g.enlace(g.estado("C"), g.estado("E"));
+        //g.enlace(g.estado("C"), g.estado("F"));//SE
+        g.enlace(g.estado("D"), g.estado("E"));
+        g.enlace(g.estado("D"), g.estado("F"));
+        //g.enlace(g.estado("D"), g.estado("G"));//SE
+        //g.enlace(g.estado("D"), g.estado("H"));//SE
+        g.enlace(g.estado("E"), g.estado("H"));
+        g.enlace(g.estado("F"), g.estado("H"));
+        g.enlace(g.estado("F"), g.estado("G"));
+    
+        AlgoritmosCarlos obj = new AlgoritmosCarlos();
+        obj.RecorridoGuiado(g,g.estado("A"), g.estado("H"));
         
         /*System.out.println("Lista de adyacencia Nodo 1");
         g.elementosLista("Nodo1");
@@ -80,8 +86,6 @@ public class Grafos {
 >>>>>>> eed299a788e1003e93de0ae57aa6832c530adbda
         System.out.println("Lista de adyacencia Nodo 1");
         g.elementosLista("Nodo1");*/
-        AlgoritmosCarlos obj = new AlgoritmosCarlos();
-        obj.ProEnProfundidad(g,g.estado("A"), g.estado("H"));
         
     }
     
